c0c1f2456da99185eff279a31adaa03c5f6746b5
Chris <christian.tiquis@gmail.com>
2025-06-06 15:02:29 +0800

    07: Dashboard Navbar Rework

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -74,7 +74,12 @@ export const DashboardUserButton = () => {
             <DrawerDescription>{data.user.email}</DrawerDescription>
           </DrawerHeader>
           <DrawerFooter>
-            <Button variant="outline" onClick={() => {}}>
+            <Button
+              variant="outline"
+              onClick={() => {
+                //TODO: Implement billing navigation
+              }}
+            >
               <CreditCardIcon className="size-4 text-black" />
               Billing
             </Button>
@@ -118,7 +123,12 @@ export const DashboardUserButton = () => {
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
+        <DropdownMenuItem
+          className="cursor-pointer flex items-center justify-between"
+          onClick={() => {
+            //TODO: Implement billing navigation
+          }}
+        >
           Billing
           <CreditCard className="size-4" />
         </DropdownMenuItem>
