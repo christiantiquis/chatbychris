0cb2302dae80c7f4dab56d93ec959e28ba59cebc
Chris <christian.tiquis@gmail.com>
2025-06-06 13:51:45 +0800

    06: Dashboard Sidebar Rework

--- src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -22,7 +22,7 @@ const firstSection = [
   { icon: VideoIcon, label: "Meetings", href: "/meetings" },
   { icon: BotIcon, label: "Agents", href: "/agents" },
 ];
-const secondSection = [{ icon: StarIcon, label: "Upgrade", href: "/meetings" }];
+const secondSection = [{ icon: StarIcon, label: "Upgrade", href: "/upgrade" }];
 
 const DashboardSidebar = () => {
   const pathname = usePathname();
@@ -46,9 +46,9 @@ const DashboardSidebar = () => {
                   <SidebarMenuButton
                     asChild
                     className={cn(
-                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                      "h-10 hover:bg-gradient-to-r border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent via-sidebar/50 to-sidebar/50",
                       pathname === item.href
-                        ? "bg-linear-to-r/oklch border-[#5D6B68]/10"
+                        ? "bg-gradient-to-r border-[#5D6B68]/10"
                         : "bg-transparent"
                     )}
                     isActive={pathname === item.href}
@@ -76,9 +76,9 @@ const DashboardSidebar = () => {
                   <SidebarMenuButton
                     asChild
                     className={cn(
-                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                      "h-10 hover:bg-gradient-to-r border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent via-sidebar/50 to-sidebar/50",
                       pathname === item.href
-                        ? "bg-linear-to-r/oklch border-[#5D6B68]/10"
+                        ? "bg-gradient-to-r border-[#5D6B68]/10"
                         : "bg-transparent"
                     )}
                     isActive={pathname === item.href}
