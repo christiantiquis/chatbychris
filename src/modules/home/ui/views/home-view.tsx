9444cc1d5e64b8387180440955835fecc6942420
Chris <christian.tiquis@gmail.com>
2025-06-07 19:36:02 +0800

    09: Agents Setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "World" }));
-  return <div className="p-4">{data?.greeting}</div>;
+  return <div className="p-4">Home View</div>;
 };
 
 export default HomeView;
